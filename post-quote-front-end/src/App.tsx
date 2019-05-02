b60ef8fbf485ddb985474357befaea823b97baf7
Alexander Davis <alexanderdavis.net@googlemail.com>
2019-05-02 17:07:23 +0100

    fetch quote

--- post-quote-front-end/src/App.tsx
+++ post-quote-front-end/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [data, setData] = useState("");
+	const [request, setRequest] = useState({
+		pickupPostcode: "SW1A1AA",
+		deliveryPostcode: "EC2A3LT",
+		vehicle: "bicycle"
+	});
+
+	async function fetchQuote() {
+		const res = await fetch("quote", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(request)
+		});
+		const data = await res.json();
+		console.log(data);
+		setData(data);
+	}
+
+	useEffect(() => {
+		fetchQuote();
+	}, [request]);
+	return <div className="App">{JSON.stringify(data)}</div>;
+};
 
 export default App;
